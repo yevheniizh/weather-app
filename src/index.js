049a74f9e3b745737331fdf1dae2b78578e82b60
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-05 15:23:37 +0200

    add App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
-  document.getElementById("root")
-);
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
