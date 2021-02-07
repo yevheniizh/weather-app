19c4ed139785d400589f56ede486a0c69f2b2bc0
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-07 15:11:07 +0200

    change request to async/await :Card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,36 +3,42 @@ import React, { useEffect, useState } from 'react';
 function Card({ city = 'Kiev' }) {
   const [weatherNowIconURL, setWeatherNowIconURL] = useState('');
   const [weatherNowDescription, setWeatherNowDescription] = useState('');
-  const [isSending, setIsSending] = useState(false);
+  const [weatherNowTemperature, setWeatherNowTemperature] = useState('');
+
   const { REACT_APP_API_URL, REACT_APP_CLIENT_KEY } = process.env;
   const urlWeather = `${REACT_APP_API_URL}/data/2.5/weather?q=${city}&units=metric&APPID=${REACT_APP_CLIENT_KEY}`;
 
   useEffect(() => {
-    fetch(urlWeather)
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherNowIcon = data.weather[0].icon;
-        const weatherNowIconURL =
-          'http://openweathermap.org/img/w/' + weatherNowIcon + '.png';
-        const weatherNowDescription = data.weather[0].description;
+    updateData();
+  }, []); // eslint-disable-line
 
-        setWeatherNowDescription(weatherNowDescription);
-        setWeatherNowIconURL(weatherNowIconURL);
-      })
-      .catch((err) => {
-        setWeatherNowDescription("Can't get description");
-      });
-    setIsSending(false);
-  }, [isSending]); // eslint-disable-line
+  async function updateData() {
+    const response = await fetch(urlWeather);
+    const data = await response.json();
+
+    const weatherNowIcon = data.weather[0].icon;
+    const weatherNowIconURL =
+      'http://openweathermap.org/img/wn/' + weatherNowIcon + '@4x.png';
+    const weatherNowDescription = data.weather[0].description;
+    weatherNowDescription[0].toUpperCase();
+
+    const weatherNowTemperature = data.main.temp;
+
+    setWeatherNowDescription(weatherNowDescription);
+    setWeatherNowIconURL(weatherNowIconURL);
+    setWeatherNowTemperature(weatherNowTemperature);
+    console.log(data);
+  }
 
   return (
     <div>
-      <input type="input" id="input"></input>
-      <button type="button" onClick={() => setIsSending(true)}>
-        Update info
-      </button>
+      <h1>{city}</h1>
       <img alt="weatherNowIcon" src={weatherNowIconURL} />
-      <h1>{weatherNowDescription}</h1>
+      <div>{weatherNowTemperature}</div>
+      <div>{weatherNowDescription}</div>
+      <button type="button" onClick={updateData}>
+        Update
+      </button>
     </div>
   );
 }
