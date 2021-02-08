065a2192866205a25d1a48c733ba416e58dbdeda
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-08 22:39:06 +0200

    add rendering cities from storage :CardContainer

--- src/components/card-container/card-container.js
+++ src/components/card-container/card-container.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Card from '../card';
 
 import styles from './card-container.module.scss';
 
-function CardContainer() {
+function CardContainer({ cities }) {
+  if (Object.entries(cities).length === 0 && cities.constructor === Object) {
+    return (
+      <div>
+        <h1>Add the first city</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={styles['card-container']}>
-      <Card />
-      <Card city={'Kharkiv'} />
-      <Card city={'New York'} />
-      <Card city={'Oslo'} />
+      {Object.values(cities).map((city) => (
+        <Card key={city} city={city} />
+      ))}
     </div>
   );
 }
 
-export default CardContainer;
+const mapStateToProps = (state) => ({
+  cities: state.cities,
+});
+
+export default connect(mapStateToProps)(CardContainer);
