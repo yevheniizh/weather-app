f2130b3ca5e36ad6bb052bc52b972a6cb463eb84
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-10 22:16:33 +0200

    add full-weather-info-container

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,19 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import MainPage from '../../pages/main-page';
 
+import FullWeatherInfoContainer from '../full-weather-info-container';
+
 const App = () => {
   return (
     <div>
       <Switch>
         <Redirect exact from="/" to="/weather-app" />
         <Route path="/weather-app" exact component={MainPage} />
+        <Route
+          path="/weather-app/:city"
+          exact
+          component={FullWeatherInfoContainer}
+        />
       </Switch>
     </div>
   );
---
+++ src/components/full-weather-info-container/full-weather-info-container.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+function FullWeatherInfoContainer() {
+  const { city } = useParams();
+
+  const [forecastList, setForecastList] = useState(null);
+
+  const { REACT_APP_API_URL, REACT_APP_CLIENT_KEY } = process.env;
+  const urlWeather = `${REACT_APP_API_URL}/data/2.5/forecast?q=${city}&units=metric&APPID=${REACT_APP_CLIENT_KEY}`;
+
+  useEffect(() => {
+    fetch(urlWeather)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        const oneDayForecast = data.list.slice(0, 8);
+        const forecastList = oneDayForecast.reduce((acc, item) => {
+          acc[item.dt_txt] = item.main.temp;
+
+          return acc;
+        }, {});
+
+        setForecastList(getForecastBody(forecastList));
+      });
+  }, []); // eslint-disable-line
+
+  const getForecastBody = (data) => {
+    const temperatureList = Object.values(data);
+    const chartHeight = 100;
+    const maxValue = Math.max(...temperatureList);
+
+    const getTemperatureList = temperatureList
+      .map((item) => {
+        const scale = chartHeight / maxValue;
+        const percent = ((item / maxValue) * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(
+          item * scale
+        )}" data-tooltip="${percent}%">${item}&deg;</div>`;
+      })
+      .join('');
+
+    return { __html: getTemperatureList };
+  };
+
+  return (
+    <div className="FullWeatherInfoContainer">
+      <div>
+        <div>City is {city}</div>
+        <div>Full Weather Info Container</div>
+        <div dangerouslySetInnerHTML={forecastList} />
+      </div>
+      <Link to={`/`}>
+        <button type="button" className="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default FullWeatherInfoContainer;
