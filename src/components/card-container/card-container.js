37238266905678893b3107fc61c75ca2da0114d2
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-10 10:49:23 +0200

    add react transition group to Cards :CardContainer

--- src/components/card-container/card-container.js
+++ src/components/card-container/card-container.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import Card from '../card';
 
 import styles from './card-container.module.scss';
+import animationStyles from './card-container-animation.module.scss';
 
 function CardContainer({ cities }) {
   if (Object.entries(cities).length === 0 && cities.constructor === Object) {
@@ -15,11 +17,13 @@ function CardContainer({ cities }) {
   }
 
   return (
-    <div className={styles['card-container']}>
+    <TransitionGroup className={styles['card-container']}>
       {Object.values(cities).map((city) => (
-        <Card key={city} city={city} />
+        <CSSTransition key={city} timeout={500} classNames={animationStyles}>
+          <Card key={city} city={city} />
+        </CSSTransition>
       ))}
-    </div>
+    </TransitionGroup>
   );
 }
 
